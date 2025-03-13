abb910b87d33fb1b0505d1a950c77a3fa1ceceb1
nd2k <nicolas.decat@gmail.com>
2025-03-13 23:19:07 +0100

    Initial commit

---
+++ src/main/java/com/nd2k/fast_payment_system/FastPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nd2k.fast_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastPaymentSystemApplication.class, args);
+	}
+
+}
